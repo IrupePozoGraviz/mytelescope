73c3ae3fac67cf8634ca64f6adb4ea9765cd099d
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-12-08 13:38:22 +0100

    börjat med responsivitet

--- my-react-app/src/pages/home.js
+++ my-react-app/src/pages/home.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import { TrialBtn } from '../components/trialBtn';
-import Girl from '../images/girl.webp'
+import Girl from '../images/girl.svg'
 import Star from '../images/stjärna.svg'
 import Computer from '../images/nyDator.webp'
 import '../css/home.css';
 
 export const Home = () => (
   <main className="home-parent">
-    <section className="home-child">
+    <section className="home-mobile">
 <h1 className="home-mobile">Forecast the future</h1>
-      <h2 className="home-hOne">Explore. Forecast.</h2> 
-      <h3 className="home-hTwo">Track market trends.</h3>
       <article className="home-mobileP">
         <p>
         Get an instant overview of current demand and forecast where it’s heading.
 Create your MyTelescope future index and monitor how you are creating an impact on the future.
         </p>
       </article>
-      <article className="home-sibling">
-        <p>
-          This is a simple React app that I created to learn how to use React. 
-          It uses React Router to navigate between pages. 
-          It also uses React Bootstrap for styling and layout.
-        </p>
-      </article>
-<div className="home-images">
-<img src={Girl} className="home-girl" alt="Girl with phone" />
-<img src={Star} className="home-star" alt="Star" />
-</div>
-    </section>
-    <div className="home-button">
-    <TrialBtn />
-    </div>
+      
+
     <div className="home-computer-mobile">
     <img src={Computer} className="home-comp" alt="Computer" />
     </div>
@@ -41,9 +26,28 @@ Create your MyTelescope future index and monitor how you are creating an impact 
         There's an average correlation of 83% between Share of Search methodology with sales, revenue, and market share. It means using branded search data as a data set replacing that when there's a surge in searches, there's a corresponding increase in the number of people purchasing the product - Look for the trend line
         </p>
         </article>
+    </section>
 
-        <blog className="home-blog">
-        <h5 className="home-blog-h5">Blog</h5>
-        </blog>
+<section className="home-biggerScreen">
+  <div className="home-bigger-child">
+<h2 className="home-hOne">Explore. Forecast.</h2> 
+      <h3 className="home-hTwo">Track market trends.</h3>
+      
+<article className="home-sibling">
+        <p>
+        Use MyTelescope AI-powered platform to generate trend dashboards for companies, brands, products, topics and insights for content creations with an impressive average accuracy of 83% by analyzing Share of Search data.
+        </p>
+      </article>
+      </div>
+      <div className="home-images">
+<img src={Girl} className="home-girl" alt="Girl with phone" />
+<img src={Star} className="home-star" alt="Star" />
+</div>
+      </section>
+  
+    <div className="home-button">
+    <TrialBtn />
+    </div>
+    
   </main>
 );
