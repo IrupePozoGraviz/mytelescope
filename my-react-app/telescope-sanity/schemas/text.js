5f1c9e5aec6bde517d4a2db42fa47b5b88cc4c90
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2024-01-17 15:36:42 +0100

    inlaggd text

---
+++ my-react-app/telescope-sanity/schemas/text.js
@@ -0,0 +1,18 @@
+// schemas/text1.js
+export default {
+    name: 'text1',
+    title: 'Text 1',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'text',
+        title: 'Text',
+        type: 'text',
+      },
+    ],
+  };
